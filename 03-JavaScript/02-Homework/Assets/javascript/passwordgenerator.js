163a6fa9203d988cf79d47812f16ae81296752d4
Lily Chon <LChon@Perkinscoie.com>
2019-10-03 23:28:03 -0700

    Need to work on randomizing user input password sequence + filter with length

--- 03-JavaScript/02-Homework/Assets/javascript/passwordgenerator.js
+++ 03-JavaScript/02-Homework/Assets/javascript/passwordgenerator.js
@@ -1,58 +1,67 @@
-//put this in btn.onlick function for generatePassword
 var length;
-var special;
-var number;
-var lower;
-var upper;
-var rPassword = document.getElementById("password");
-// how to randomize a string or array
-var charTypes = ["@", "%", "+", "'", "!", "#", "$", "^", "?", ":", ".", "~", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 0,1,2,3,4,5,6,7,8,9]
-var characters = [];
-var passwordArray = [];
-
-const MIN = 8;
-const MAX = 128;
+var specials = '!"#$%&()*+,-./:;<=>?@[\]^_`{|~';
+var numbers = '1234567890';
+var lowers = 'abcdefghijklmnopqrstuvwxyz';
+var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var passBox = document.getElementById("password");
+var rPassword = '';
 
 function generatePassword() {
-    length = prompt("Choose a length between 8 and 128");
-    alertThis("Length = " + length);
+    rPassword = "";
+    
+    while (length !== null) {
+        length = prompt("Choose a length between 8 and 128");
+        
+        if (length >=8 && length <=128) {
+        alertThis("Length = " + length); 
+        placeholder = true;
+        }   
+        else if (isNaN(length)) {
+        prompt("It is not a number. Please enter a number between 8 and 128");
+        } 
+        else {
+        prompt("You need to choose a number between 8 and 128");
+        }
+    }
     alertThis("Please choose at least 1 Character type.")
-    special = confirm("Would you like Special characters (!#$%&'()*+,-./:;<=>?@[\]^_`{|}~) in your random password?");
-    number = confirm("Would you like Numeric Characters (0123456789) in your random password?");
-    lower = confirm("Would you like Lowercase Characters (abcdefghijklmnopqrstuvwxyz) in your random password?");
-    upper = confirm("Would you like Uppercase Characters (ABCDEFGHIJKLMNOPQRSTUVWXYZ) in your random password?");
+    var special = confirm("Would you like Special characters (!#$%&'()*+,-./:;<=>?@[\]^_`{|}~) in your random password?");
+    var number = confirm("Would you like Numeric Characters (0123456789) in your random password?");
+    var lower = confirm("Would you like Lowercase Characters (abcdefghijklmnopqrstuvwxyz) in your random password?");
+    var upper = confirm("Would you like Uppercase Characters (ABCDEFGHIJKLMNOPQRSTUVWXYZ) in your random password?");
 
-    rPassword.innerHTML = "";
-    passwordArray=[];
-    characters = charTypes;
-    const pwLength = parseInt(length.value);
 
+
+    if (special === false && number === false && lower === false && upper === false) {
+        alert("You must choose at least 1 Character type.");
+    } 
     if (special === true) {
-        characters = characters.join("").replace(/[@%+'!#$^?:.~]/g,'').split('')
-    }
+        // add special to password
+        rPassword += specials;
+    } 
     if (number === true) {
-        characters = characters.join("").replace(/[0-9]/g,'').split('')
-    }
+        // add number to password
+        rPassword += numbers;
+    } 
     if (lower === true) {
-        characters = characters.join("").replace(/[a-zA-Z]/g,'').split('')
-    }
+        // add lower to password
+        rPassword += lowers;
+    } 
     if (upper === true) {
-        characters = characters.join("").replace(/[1Ilo0]/g,'').split('')
+        // add upper to password
+        rPassword += uppers;
     }
 
-    if (pwLength < MIN || pwLength > MAX) {
-        errorMessage.innerHTML = "You must pick a password between " + MIN + " and " + MAX;
-        return;
-    }
+    
+    
+
+    
+    passBox.innerHTML = rPassword;
 }
+
 
 function alertThis(message) {
     alert(message);
 }
-
-// var generateBtn = document.getElementById("generate");
-// var copyBtn = document.getElementById("copy");
-// var passwordText = document.getElementById("password");
 
 function copyToClip() {
 	var value = document.getElementById("password").innerHTML;
@@ -65,6 +74,3 @@ function copyToClip() {
 
 	alert("Password copied!");
 }
-
-function getMin() {return MIN;}
-function getMax () {return MAX;}
