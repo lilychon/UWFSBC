59e9fe425c058e41ee6d821ded84acff51176c1c
Lily Chon <lchon@perkinscoie.com>
2019-10-23 17:24:27 -0700

    I cannot figure out how to add the icons to the for loop of blue boxes at the bottom

--- 06-Server-Side-APIs/02-Homework/script.js
+++ 06-Server-Side-APIs/02-Homework/script.js
@@ -4,6 +4,10 @@ var cities = [];
 
 $(".searchBtn").on("click", function (event) {
     event.preventDefault();
+    displayWeatherInfo();
+})
+
+function displayWeatherInfo() {
     $(".fiveDay").empty();
     var city = $("#cityInput").val();
 
@@ -26,30 +30,99 @@ $(".searchBtn").on("click", function (event) {
             const currentDate = moment().format("M/D/YYYY");
 
             $(".city").html("<h2>" + response.city.name + " (" + currentDate + ")" + "</h2>");
-            $(".temp").text("Temperature: " + response.list[0].main.temp.toFixed(1) + " °F");
-            $(".humidity").text("Humidity: " + response.list[0].main.humidity + "%");
-            $(".wind").text("Wind Speed: " + response.list[0].wind.speed.toFixed(1) + " MPH");
-            $(".uvIndex").html("UV Index: " + "<p>" + uvResponse.value + "</p>");
+
+            // $(".city").appendTo(weather());
+
+            if (response.list[0].weather[0].main === "Clear") {
+                $("<i>", {
+                    class: "fas fa-sun weatherIcon",
+                    appendTo: ".city"
+                    
+                })
+            }
+            else if (response.list[0].weather[0].main === "Clouds") {
+                $("<i>", {
+                    class: "fas fa-cloud weatherIcon",
+                    appendTo: ".city"
+                    
+                })
+            }
+            else if (response.list[0].weather[0].main === "Snow") {
+                $("<i>", {
+                    class: "fas fa-snowflake weatherIcon",
+                    appendTo: ".city"
+                    
+                })
+            }
+            else if (response.list[0].weather[0].main === "Drizzle") {
+                $("<i>", {
+                    class: "fas fa-cloud-rain weatherIcon",
+                    appendTo: ".city"
+                    
+                })
+            }
+            else if (response.list[0].weather[0].main === "Rain") {
+                $("<i>", {
+                    class: "fas fa-cloud-showers-heavy weatherIcon",
+                    appendTo: ".city"
+                    
+                })
+            }
+
+            $(".temp").html("<p>" + "Temperature: " + response.list[0].main.temp.toFixed(1) + " °F" + "</p>");
+            $(".humidity").html("<p>" + "Humidity: " + response.list[0].main.humidity + "%" + "</p>");
+            $(".wind").html("<p>" + "Wind Speed: " + response.list[0].wind.speed.toFixed(1) + " MPH" + "</p>");
+            $(".uvIndex").html("UV Index: " + "<h4>" + uvResponse.value + "</h4>");
 
             var cityHistory = $("<button>" + response.city.name + "</button>");
-            cityHistory.addClass("btn btn-light w-100");
+            cityHistory.addClass("btn btn-light w-100 cityBtn");
             $(".cityHistory").append(cityHistory);
 
 
             for (var i = 1; i < 6; i++) {
                 const date = moment().add(i, 'days').format("M/D/YYYY");
                 $("<div>", {
-                    html: "<h3>" + date + "</h3>" + "Temp: " + response.list[i].main.temp.toFixed(1) + " °F" + "<br>" + "Humidity: " + response.list[i].main.humidity.toFixed(1) + "%",
+                    html: "<h3>" + date + "<br>" + "</h3>" + "Temp: " + response.list[i].main.temp.toFixed(1) + " °F" + "<br>" + "Humidity: " + response.list[i].main.humidity.toFixed(1) + "%",
                     id: "fiveDayBox",
                     appendTo: ".fiveDay"
                 })
-
             }
             // cityHistory.onclick("click", function(history) {
             //     history.preventDefault();
             //     event(); 
-            
+            // })
+
+            // function weather() {
+            //     if (response.list[0].weather[0].main === "Clear") {
+            //         $("<i>", {
+            //             class: "fas fa-sun weatherIcon"
+            //         })
+            //     }
+            //     else if (response.list[0].weather[0].main === "Clouds") {
+            //         $("<i>", {
+            //             class: "fas fa-cloud weatherIcon"
+            //         })
+            //     }
+            //     else if (response.list[0].weather[0].main === "Snow") {
+            //         $("<i>", {
+            //             class: "fas fa-snowflake weatherIcon"
+            //         })
+            //     }
+            //     else if (response.list[0].weather[0].main === "Drizzle") {
+            //         $("<i>", {
+            //             class: "fas fa-cloud-rain weatherIcon"
+            //         })
+            //     }
+            //     else if (response.list[0].weather[0].main === "Rain") {
+            //         $("<i>", {
+            //             class: "fas fa-cloud-showers-heavy weatherIcon"
+            //         })
+            //     }
+            // }
+
         })
     })
-})
+}
+
+$(document).on("click", ".cityBtn", displayWeatherInfo);
 
